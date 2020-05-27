cde82b2b25bd56227d83cfcb926ff3ba3817bd0f
GedMarc <ged_marc@hotmail.com>
2020-05-27 02:31:49 +0200

    Building after lint and removing cxf bundle for each dependency separately to keep in sync

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -3,7 +3,6 @@ module com.jwebmp.plugins.jqgradientlinear {
 
 	requires com.jwebmp.core;
 	requires com.guicedee.logmaster;
-	requires com.fasterxml.jackson.annotation;
 
 	requires java.validation;
 	requires java.logging;
